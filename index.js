34467bb43aaaca5240b085445d8ae9e654d6649d
Mary Jack <maryjackinthebox@gmail.com>
2018-11-04 22:38:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  var customerName = 'bob';
  
  function upperCaseCustomerName(){
-   customerName.toUpperCase;
+   let upperCaseCustomerName = customerName.toUpperCase;
  }
  
 function setBestcustomer(){
